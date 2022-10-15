8534c6d380b4b852f1ff764e66b39f4d3ccd44f7
Paul-Desii <pauldeskidial@gmail.com>
2022-10-15 02:28:36 +0100

    Updates

---
+++ precache-manifest.3eab5a77efdf81b72b34842a8201fafc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffac0de98ebfb0f1e46ba9389912f16c",
+    "url": "/keeperapp/index.html"
+  },
+  {
+    "revision": "d8bbe94d9aab3969a360",
+    "url": "/keeperapp/static/js/2.6ee16f53.chunk.js"
+  },
+  {
+    "revision": "87715326a78ea0544d91",
+    "url": "/keeperapp/static/js/main.c8ac1c7e.chunk.js"
+  },
+  {
+    "revision": "f604907ce0e60a3c773c",
+    "url": "/keeperapp/static/js/runtime-main.86e75e36.js"
+  }
+]);
